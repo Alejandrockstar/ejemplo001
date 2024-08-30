d77a974d39dff543b1008f2f30f52d145c6081f0
Alejandro López Vázquez <127002619+Alejandrockstar@users.noreply.github.com>
2024-08-30 00:10:03 +0000

    Crear archivo String

---
+++ metodos_string.rb
@@ -0,0 +1,38 @@
+# Métodos De String
+# Alejandro López Vázquez
+
+# string_methods.rb
+
+# Método para convertir una cadena a mayúsculas
+def convertir_a_mayusculas(cadena)
+    cadena.upcase
+  end
+  
+  # Método para convertir una cadena a minúsculas
+  def convertir_a_minusculas(cadena)
+    cadena.downcase
+  end
+  
+  # Método para contar el número de caracteres en una cadena
+  def contar_caracteres(cadena)
+    cadena.length
+  end
+  
+  # Método para verificar si una cadena contiene una subcadena
+  def contiene_subcadena?(cadena, subcadena)
+    cadena.include?(subcadena)
+  end
+  
+  # Método para reemplazar una subcadena en una cadena
+  def reemplazar_subcadena(cadena, subcadena_antigua, subcadena_nueva)
+    cadena.gsub(subcadena_antigua, subcadena_nueva)
+  end
+  
+  # Ejemplo de uso
+  puts convertir_a_mayusculas("hola mundo")                # => "HOLA MUNDO"
+  puts convertir_a_minusculas("HOLA MUNDO")                # => "hola mundo"
+  puts contar_caracteres("hola")                          # => 4
+  puts contiene_subcadena?("hola mundo", "mundo")         # => true
+  puts reemplazar_subcadena("hola mundo", "mundo", "universo")  # => "hola universo"
+  
+  
